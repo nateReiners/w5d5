ec03aa9d7b5ad3d2ee31eb89d278346912bd8bf5
Nate Reiners <nate.reiners@gmail.com>
2016-12-16 14:00:28 -0800

    Refactored curry, completed currySpread method

--- currySpread.js
+++ currySpread.js
@@ -1,11 +1,12 @@
 Function.prototype.curry = function(numArgs) {
-
   const arr = [];
   const that = this;
   let total = 0;
 
-  function callbackFunction(arg) {
-    arr.push(arg);
+  function callbackFunction(...args) {
+    args.forEach(function(n) {
+      arr.push(n);
+    });
 
     if (arr.length === numArgs) {
       return that.apply(that, arr);
@@ -25,4 +26,5 @@ console.log(sumThree(4, 20, 6)); // == 30
 
 // you'll write `Function#curry`!
 
+console.log(sumThree.curry(3)(4, 20, 6)); // == 30
 console.log(sumThree.curry(3)(4)(20)(6)); // == 30
