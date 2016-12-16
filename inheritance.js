b09e03b7842abb62112ef6f2412b294f4262760f
Nate Reiners <nate.reiners@gmail.com>
2016-12-16 14:14:41 -0800

    Function.prototype.inherits method

---
+++ inheritance.js
@@ -0,0 +1,6 @@
+Function.prototype.inherits = function(parentClass) {
+  function Surrogate() {}
+  Surrogate.prototype = parentClass.prototype;
+  this.prototype = new Surrogate();
+  this.prototype.constructor = this;
+};
